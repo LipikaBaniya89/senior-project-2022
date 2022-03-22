f66b94b5731a2b69b48aa6c635dbed4642ea6d0c
Lipika Baniya <89762072+LipikaBaniya89@users.noreply.github.com>
2022-03-22 18:18:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 require('./db/db.js')
 
 //Router
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var surveysRouter = require ('./routes/survey');
 var imagesRouter = require('./routes/image');
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/surveys', surveysRouter);
 app.use('/images', imagesRouter);
@@ -37,6 +37,12 @@ app.use('/customers',customersRouter);
 app.use('/customers_point',customersPointRouter);
 app.use('/answers',answersRouter);
 
+app.use(express.static(path.join(__dirname, "public", "react-senior"))); 
+
+app.get("/*", function (req, res) { 
+  res.sendFile(path.join(__dirname, "public", "react-senior", "index.html")); 
+
+}); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
